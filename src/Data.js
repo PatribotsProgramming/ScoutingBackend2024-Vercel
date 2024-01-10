25751fe461f68ba055143bf344c87b153e1c427f
RudyG252 <122180943+RudyG252@users.noreply.github.com>
2024-01-10 01:15:15 -0800

    last commit didn't work?

--- src/Data.js
+++ src/Data.js
@@ -153,3 +153,70 @@ function convertToTableForm(data) {
   
   return table;
 }
+
+
+
+function getIndividualDatapoints(data) {
+  let datapoints = [];
+  // bots in first match (designated by r1, r2, r3, b1, b2, b3)
+  let keys2 = Object.keys(data[0]);
+  // if there are no matches, return empty table
+  if (keys2.length == 0) {
+    return table;
+  }
+  // gets all the data points using the data from the first bot in the first match
+  let keys = Object.keys(data[0][keys2[0]]);
+  // pushes those data points to the first row of the table (the header)
+  for (let i = 0; i < keys.length; i++) {
+    datapoints.push(keys[i]);
+  }
+  return datapoints;
+}
+
+function convertToTeamMap(data) {
+  // overall map structure
+  let teamMap = new Map();
+  const points = getIndividualDatapoints(rawData);
+  for (let i = 1; i < data.length; i++) {
+    if (!teamMap.has(data[i][0])) {
+      teamMap.set(data[i][0], [data[0], data[i]]);
+    }
+    else {
+      teamMap.get(data[i][0]).push(data[i]);
+    }
+  }
+  console.log(teamMap);
+  return teamMap;
+}
+
+
+
+
+function getTeamAverage(team) {
+  let dataMap = new Map();
+  let teamData = getTeamData(team);
+  let points = getIndividualDatapoints(rawData);
+  for (let i = 1; i < points.length; i++) {
+    dataMap.set(points[i], 0);
+  }
+  for (let i = 1; i < teamData.length; i++) {
+    for (let j = 1; j < teamData[i].length; j++) {
+      if (isNumeric(points[i])) {
+        dataMap.set(points[j], dataMap.get(points[j]) + teamData[i][j]);
+      }
+    }
+  }
+
+  for (let i = 0; i < points.length - 1; i++) {
+    if (isNumeric(points[i])) {
+      dataMap.set(points[i], dataMap.get(points[i]) / (teamData.length - 1));
+    }
+  }
+
+  console.log(dataMap);
+  return dataMap;
+}
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n) || n == true || n == false;
+}
