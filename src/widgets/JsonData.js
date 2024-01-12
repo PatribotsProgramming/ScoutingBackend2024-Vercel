c55aeb3b2890dca3ae9682e26d70aca6257b7acc
ChadPad <97857553+PatribotsProgramming@users.noreply.github.com>
2024-01-12 01:26:16 +0000

    create specific function getData to get data of a team's match in the case of missing data

--- src/widgets/JsonData.js
+++ src/widgets/JsonData.js
@@ -22,3 +22,22 @@ export async function getAllData() {
     console.error("Error fetching data: ", error);
   }
 }
+
+//function to get the data of a match a team played in the case of missing data
+export async function getData(path) {
+  try {
+    const dbRef = ref(db, path);
+    const snapshot = await get(dbRef);
+    if (snapshot.exists()) {
+      const data = snapshot.val();
+      const jsonString = JSON.stringify(data);
+      console.log(jsonString)
+      return jsonString;
+    } else {
+      console.log("No data available");
+      return null;
+    }
+  } catch (error) {
+    console.error("Error fetching data: ", error);
+  }
+}
