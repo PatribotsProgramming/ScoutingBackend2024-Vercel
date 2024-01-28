bcbb8c7df04c8f005222e5bdf14acf3fb88c786a
Oliver M. Cushman <olivermcushman@gmail.com>
2024-01-28 11:38:18 -0800

    new search func

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -2,24 +2,32 @@
 import React, { useEffect, useState } from "react";
 import {fetchDataAndProcess} from '../Data.js'
 import "./Search.css";
+import "./Tables.css";
 
 function Search() {
-    const [data, setData] = useState([]);
+    const [averageData, setAverageData] = useState([]);
+    const [matchData, setMatchData] = useState([]);
     const [team, setTeam] = useState("");
     const [value, setValue] = useState("");
     const [teamData, setTeamData] = useState([]);
+    const [teamMatchData, setTeamMatchData] = useState([]);
+    const [matchDataType, setMatchDataType] = useState("num");
 
     useEffect(() => {
         setTimeout(() => {
             fetchDataAndProcess().then((data) => {
-                setData(data.teamAverageMap);
+                setAverageData(data.teamAverageMap);
+                setMatchData(data.bigTeamMap);
             });
         }, 1000);
     }, []);
 
     useEffect(() => {
-        if (data.size !== 0 && data.size !== undefined) {
-            setTeamData(data.get(team));
+        if (averageData.size !== 0 && averageData.size !== undefined) {
+            setTeamData(averageData.get(team));
+        }
+        if (matchData.size !== 0 && matchData.size !== undefined) {
+            setTeamMatchData(matchData.get(team));
         }
         // eslint-disable-next-line
     }, [team]);
@@ -32,13 +40,19 @@ function Search() {
         setTeam(value);
     }
 
-    const onKeyDownHandler = e => {
+    const onKeyDownHandler = (e) => {
         if (e.keyCode === 13) {
             handleSearch();
-        }
+        }   
     };
 
-    if (teamData === undefined || teamData === null || teamData[0] === undefined || teamData[0].length === 0) {
+    const emptyData = (data) => {
+        return data === undefined || data[0] === undefined || data[0].length === 0
+    }
+
+    // console.log(teamMatchData);
+
+    if (emptyData(teamData) || emptyData(teamMatchData)) {
         return (
             <div className="search">
                 <div className="search-bar">
@@ -58,6 +72,24 @@ function Search() {
         );
     }
 
+    const matchContent = (matches) => {
+        if (matchDataType === "num") {
+            return matches.slice(0, 1).concat(matches.slice(7));
+        }
+        return matches.slice(0, 7);
+    }
+
+    const handleSelectChange = (e) => {
+        setMatchDataType(e.target.value);
+    }
+
+    let headers = teamData[0];
+    let stats = teamData[1];
+
+    let matchHeads = teamMatchData[0];
+
+    let matchStats = teamMatchData.slice(1);
+
     return (
         <div className="search">
             <div className="search-bar">
@@ -72,11 +104,58 @@ function Search() {
             </div>  
 
             <div className="team-stats">
-                {teamData[0].map((category, index) => (
-                    <div key={category} className="data-point">
-                        {teamData[0][index] + ": " + (Math.round(teamData[1][index] * 100) / 100)}
-                    </div>
-                ))}
+                <table className="table">
+                    {/* Render headers */}
+                    <thead>
+                        <tr>
+                            {headers.map((header, index) => (
+                                <th key={index}>{header}</th>
+                            ))}
+                        </tr>
+                    </thead>
+                    
+                    {/* Render content */}
+                    <tbody>
+                        <tr>
+                            {stats.map((cellData, index) => (
+                                <td key={index}>{cellData}</td>
+                            ))}
+                        </tr>
+                    </tbody>
+                </table>
+                <div className="match-content-selector">
+                    <select onChange={handleSelectChange}>
+                        <option value="num">
+                            Numbers
+                        </option>
+                        <option value="comment">
+                            Comments
+                        </option>
+                    </select>
+                </div>
+                <div className="match-stats-container">
+                    <table className="table">
+                        {/* Render headers */}
+                        <thead>
+                            <tr>
+                                {matchContent(matchHeads).map((header, index) => (
+                                    <th key={index}>{header}</th>
+                                ))}
+                            </tr>
+                        </thead>
+                        
+                        {/* Render data */}
+                        <tbody>
+                            {matchStats.map((rowData, rowIndex) => (
+                                <tr key={rowIndex}>
+                                    {matchContent(rowData).map((cellData, cellIndex) => (
+                                        <td key={cellIndex}>{cellData}</td>
+                                    ))}
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     );
