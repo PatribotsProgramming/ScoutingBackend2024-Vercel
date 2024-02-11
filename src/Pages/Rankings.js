03bba6ef077a8daba03cab9019c38d97efa4b0e8
Oliver M. Cushman <olivermcushman@gmail.com>
2024-02-11 13:31:18 -0800

    rechat

--- src/Pages/Rankings.js
+++ src/Pages/Rankings.js
@@ -1,7 +1,8 @@
-import { getTeamData } from "../Data";
 import React, { useEffect, useState } from "react";
+import TreeGraph from "../widgets/TreeGraph";
 import {fetchDataAndProcess} from '../Data.js'
 import "./Tables.css";
+import "./Rankings.css";
 
 function Rankings() {
     const [data, setData] = useState([]);
@@ -42,36 +43,58 @@ function Rankings() {
         return <div>Loading...</div>;
     }
 
+    const convertTreeMap = () => {
+        let arr = [];
+        for (let i = 0; i < data.rankingTable.length; i++) {
+            arr.push({
+                "name": data.rankingTable[i]["Team"],
+                "children": [
+                    {
+                        "name": data.rankingTable[i]["Team"],
+                        "Score": data.rankingTable[i]["Score"]
+                    }
+                ]
+            });
+        }
+        console.log(arr);
+        return arr;
+    }
+
     let headers = Object.keys(data.rankingTable[0]);
 
     return (
-        <div className="container">
-            <link
-                rel="stylesheet"
-                href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"
-            ></link>
-            <table className="table">
-                <thead className="header">
-                    <tr>
-                        {headers.map((header, index) => (
-                            <th key={index} onClick={() => handleSort(header)}>
-                                {header}
-                            </th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.rankingTable.map((item, index) => (
-                        <tr key={index}>
+        <div className="rankings-wrapper">
+            <div className="container">
+                <link
+                    rel="stylesheet"
+                    href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"
+                ></link>
+                <table className="table">
+                    <thead className="header">
+                        <tr>
                             {headers.map((header, index) => (
-                                <td key={index}>
-                                    {(isNaN(item[header])) ? item[header] : Math.round(item[header] * 100) / 100}
-                                </td>
+                                <th key={index} onClick={() => handleSort(header)}>
+                                    {header}
+                                </th>
                             ))}
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {data.rankingTable.map((item, index) => (
+                            <tr key={index}>
+                                {headers.map((header, index) => (
+                                    <td key={index}>
+                                        {(isNaN(item[header])) ? item[header] : Math.round(item[header] * 100) / 100}
+                                    </td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+            <div className="tree-chart">
+                <TreeGraph data={convertTreeMap()} dataKey={"Score"}/>
+            </div>
         </div>
     );
 }
---
+++ src/widgets/TreeGraph.js
@@ -0,0 +1,22 @@
+import {
+    Treemap
+} from "recharts";
+
+const TreeGraph = (props) => {
+    return (
+        <div>
+            <Treemap
+                width={window.innerWidth- 15}
+                height={320}
+                data={props.data}
+                dataKey={props.dataKey}
+                aspectRatio={4 / 3}
+                stroke="#d4af37"
+                fill="#282828"
+                // isAnimationActive={false}y
+            />
+        </div>
+    );
+};
+
+export default TreeGraph;
